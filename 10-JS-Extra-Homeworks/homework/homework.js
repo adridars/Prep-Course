dc47990bfef26bfed99b808f8fb04f8a76e5d31e
Adrián Darsie <adrian_darsie@hotmail.com>
2022-04-08 09:35:43 -0300

    versión 1

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -4,12 +4,12 @@ function deObjetoAmatriz(objeto){
   // Escribe una función que convierta un objeto en una matriz, donde cada elemento representa 
   // un par clave-valor en forma de matriz.
   //Ejemplo: 
-  /*objeto({
-      D: 1,
-      B: 2,
-      C: 3
-    }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
+  /*objeto({ D: 1, B: 2, C: 3}) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  let array = []
+  for (let a in objeto) {
+    array.push([a, objeto[a]]);
+  } return array;
 }
 
 
